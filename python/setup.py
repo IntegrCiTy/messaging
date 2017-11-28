58b4b241ba63784f7e3d50abb78e768b92c5d56d
Gillian Basso <gillian.basso@epfl.ch>
2017-11-28 16:16:04 +0100

    remove test info

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ setup(name="ict-messaging",
       maintainer="The OBNL Team",
       maintainer_email="gillian.basso@hevs.ch",
       url="https://github.com/IntegrCiTy/messaging",
-      version="0.8.0",
+      version="0.8.1",
       platforms=[platform.platform()],  # TODO indicate really tested platforms
 
       packages=["ict.protobuf"],
